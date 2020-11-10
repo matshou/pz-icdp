bf0946291d1daf2a4c0eb8ff042141258e6118bd
Matthew Cain <yooksi@zoho.com>
2020-11-10 19:44:05 +0100

    Fix volume math operations

--- lua/client/SVICDP_CDPlayer_VolumeControl.lua
+++ lua/client/SVICDP_CDPlayer_VolumeControl.lua
@@ -15,7 +15,7 @@ function ICDPKeysUp(keynum)
 			local ICDPCDplayerData = cd_player:getModData();
 
 			cdplayer_volume = getCDPlayerVolume(ICDPCDplayerData);
-			cdplayer_volume = math.max(tonumber(string.format("%.1f", cdplayer_volume + 0.1)), 1);
+			cdplayer_volume = math.min(tonumber(string.format("%.1f", cdplayer_volume + 0.1)), 1);
 
 			ICDPCDplayerData.Volume = cdplayer_volume;
 
@@ -25,7 +25,7 @@ function ICDPKeysUp(keynum)
 			local ICDPCDplayerData = cd_player:getModData();
 
 			cdplayer_volume = getCDPlayerVolume(ICDPCDplayerData);
-			cdplayer_volume = math.min(tonumber(string.format("%.1f", cdplayer_volume - 0.1)), 0);
+			cdplayer_volume = math.max(tonumber(string.format("%.1f", cdplayer_volume - 0.1)), 0);
 
 			ICDPCDplayerData.Volume = cdplayer_volume;
 		end
